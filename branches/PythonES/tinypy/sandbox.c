5a236ddf09ece32bcb92c00ad4a1d0b4a94ec54a
andy55 <andy55@c0c488f3-8e49-0410-9d99-171a865aaac7>
2008-11-11 00:23:04 +0000

    created new branch

---
+++ branches/PythonES/tinypy/sandbox.c
@@ -0,0 +1,67 @@
+void tp_sandbox(TP, double time_limit, unsigned long mem_limit) {
+    tp->time_limit = time_limit;
+    tp->mem_limit = mem_limit;
+}
+
+void tp_mem_update(TP) {
+    if((!tp->mem_exceeded) &&
+       (tp->mem_used > tp->mem_limit) && 
+       (tp->mem_limit != TP_NO_LIMIT)) {
+        tp->mem_exceeded = 1;
+        tp_raise(,tp_string("(tp_mem_update) SandboxError: memory limit exceeded"));
+    }
+}
+
+void tp_time_update(TP) {
+    clock_t tmp = tp->clocks;
+    if(tp->time_limit != TP_NO_LIMIT)
+    {
+        tp->clocks = clock();
+        tp->time_elapsed += ((double) (tp->clocks - tmp) / CLOCKS_PER_SEC) * 1000.0;
+        if(tp->time_elapsed >= tp->time_limit)
+            tp_raise(,tp_string("(tp_time_update) SandboxError: time limit exceeded"));
+    }
+}
+
+void *tp_malloc(TP, unsigned long bytes) {
+    unsigned long *ptr = (unsigned long *) calloc(bytes + sizeof(unsigned long), 1);
+    if(ptr) {
+        *ptr = bytes;
+        tp->mem_used += bytes + sizeof(unsigned long);
+    }
+    tp_mem_update(tp);
+    return ptr+1;
+}
+
+void tp_free(TP, void *ptr) {
+    unsigned long *temp = (unsigned long *) ptr;
+    if(temp) {
+        --temp;
+        tp->mem_used -= (*temp + sizeof(unsigned long));
+        free(temp);
+    }
+    tp_mem_update(tp);
+}
+
+void *tp_realloc(TP, void *ptr, unsigned long bytes) {
+    unsigned long *temp = (unsigned long *) ptr;
+    int diff;
+    if(temp && bytes) {
+        --temp;
+        diff = bytes - *temp;
+        *temp = bytes;
+        tp->mem_used += diff;
+        temp = (unsigned long *) realloc(temp, bytes+sizeof(unsigned long));
+        return temp+1;
+    }
+    else if(temp && !bytes) {
+        tp_free(tp, temp);
+        return NULL;
+    }
+    else if(!temp && bytes) {
+        return tp_malloc(tp, bytes);
+    }
+    else {
+        return NULL;
+    }
+}
